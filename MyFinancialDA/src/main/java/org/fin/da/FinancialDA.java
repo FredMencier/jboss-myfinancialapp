6f748605393278705968d5c44bfe205be109a352
Frederic Mencier <fmencier@yahoo.fr>
2024-03-26 15:42:45 +0100

    finalisation currency db

--- MyFinancialDA/src/main/java/org/fin/da/FinancialDA.java
+++ MyFinancialDA/src/main/java/org/fin/da/FinancialDA.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class FinancialDA {
@@ -21,16 +23,24 @@ public class FinancialDA {
     }
 
     public List<Currency> findCurrencies() {
-        System.out.println("----> " + entityManager);
-        CurrencyEntity currencyEntity = entityManager.find(CurrencyEntity.class, 1L);
-        System.out.println("Currency : " + currencyEntity.getCode());
+        TypedQuery<CurrencyEntity> selectCFromCurrencyEntity = entityManager.createQuery("SELECT c from CurrencyEntity c", CurrencyEntity.class);
+        List<CurrencyEntity> resultList = selectCFromCurrencyEntity.getResultList();
 
-        List<Currency> res = new ArrayList<>();
-        res.add(new Currency("CHF", "756", "Swiss franc", "Switzerland"));
-        res.add(new Currency("EUR", "978", "Euro", "France"));
-        res.add(new Currency("USD", "840)", "United States dollar", "United States"));
-        res.add(new Currency("BRL", "986", "Brazilian real", "Brazil"));
-        res.add(new Currency("JPY", "392", "Japanese yen", "Japan"));
-        return res;
+        return resultList.stream()
+                .map(currencyEntity -> new Currency(currencyEntity.getCode(), currencyEntity.getNum(), currencyEntity.getName(), currencyEntity.getCountry()))
+                .collect(Collectors.toList());
+
+
+//        System.out.println("----> " + entityManager);
+//        CurrencyEntity currencyEntity = entityManager.find(CurrencyEntity.class, 1L);
+//        System.out.println("Currency : " + currencyEntity.getCode());
+
+//        List<Currency> res = new ArrayList<>();
+//        res.add(new Currency("CHF", "756", "Swiss franc", "Switzerland"));
+//        res.add(new Currency("EUR", "978", "Euro", "France"));
+//        res.add(new Currency("USD", "840)", "United States dollar", "United States"));
+//        res.add(new Currency("BRL", "986", "Brazilian real", "Brazil"));
+//        res.add(new Currency("JPY", "392", "Japanese yen", "Japan"));
+//        return res;
     }
 }
